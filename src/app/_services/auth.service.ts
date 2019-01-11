e82bf6912134be1581df5ccf676d7774aa623ea2
aurelie <aurelie.cissokho@gmail.com>
2019-01-11 14:09:48 +0100

    creation auth.service.ts

--- src/app/_services/auth.service.ts
+++ src/app/_services/auth.service.ts
@@ -1,9 +1,42 @@
 import { Injectable } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Injectable({
-  providedIn: 'root'
+	providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+	constructor() { }
+	//new user
+	createNewUser(email: string, password: string) {
+		return new Promise((resolve, reject) => {
+			firebase.auth().createUserWithEmailAndPassword(email, password).then(
+				() => {
+					resolve();
+				},
+				(error) => {
+					reject(error);
+				}
+			);
+		})
+	}
+	//connect user
+	signInUser(email: string, password: string) {
+		return new Promise(
+			(resolve, reject) => {
+				firebase.auth().signInWithEmailAndPassword(email, password).then(
+					() => {
+						resolve();
+					},
+					(error) => {
+						reject(error);
+					}
+				);
+			}
+		);
+	}
+	//disconnect user
+	signOutUser() {
+		firebase.auth().signOut();
+	}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import * as firebase from 'firebase';
 //Services
 import { AuthService } from './_services/auth.service';
 import { AuthGuardService } from './_services/auth-guard.service';
@@ -20,36 +21,49 @@ import { BookFormComponent } from './bookList/book-form/book-form.component';
 import { HeaderComponent } from './header/header.component';
 
 const appRoutes: Routes = [
-  { path: 'auth/signup', component: SignupComponent },
-  { path: 'auth/signin', component: SigninComponent },
-  { path: 'books', component: BookListComponent },
-  { path: 'books/new', component: BookFormComponent },
-  { path: 'books/view/:id', component: SingleBookComponent }
+	{ path: 'auth/signup', component: SignupComponent },
+	{ path: 'auth/signin', component: SigninComponent },
+	{ path: 'books', component: BookListComponent },
+	{ path: 'books/new', component: BookFormComponent },
+	{ path: 'books/view/:id', component: SingleBookComponent }
 ];
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    SignupComponent,
-    SigninComponent,
-    BookListComponent,
-    SingleBookComponent,
-    BookFormComponent,
-    HeaderComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule,
-    FormsModule,
-    ReactiveFormsModule,
-    RouterModule.forRoot(appRoutes)
-  ],
-  providers: [
-    AuthService,
-    BooksService,
-    AuthGuardService
-  ],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		SignupComponent,
+		SigninComponent,
+		BookListComponent,
+		SingleBookComponent,
+		BookFormComponent,
+		HeaderComponent
+	],
+	imports: [
+		BrowserModule,
+		AppRoutingModule,
+		HttpClientModule,
+		FormsModule,
+		ReactiveFormsModule,
+		RouterModule.forRoot(appRoutes)
+	],
+	providers: [
+		AuthService,
+		BooksService,
+		AuthGuardService
+	],
+	bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+
+	constructor() {
+		const config = {
+			apiKey: "AIzaSyB4vRy9K6IS_pdXuHvo-BZaN_WtXqLgVBk",
+			authDomain: "bookangular-ad883.firebaseapp.com",
+			databaseURL: "https://bookangular-ad883.firebaseio.com",
+			projectId: "bookangular-ad883",
+			storageBucket: "",
+			messagingSenderId: "470953409162"
+		};
+		firebase.initializeApp(config);
+	}
+}
