f2f021a370bda4f4ca85d9c0b3611998467db355
aurelie <aurelie.cissokho@gmail.com>
2019-01-11 14:57:12 +0100

    creation bookService

--- src/app/_services/books.service.ts
+++ src/app/_services/books.service.ts
@@ -1,9 +1,47 @@
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+import { Book } from '../_models/book.model';
+import * as firebase from 'firebase';
+import DataSnapshot = firebase.database.DataSnapshot;
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable()
 export class BooksService {
 
-  constructor() { }
+  books: Book[] = [];
+  booksSubject = new Subject<Book[]>();
+
+  constructor() {
+    this.getBooks();
+  }
+
+  emitBooks() {
+    this.booksSubject.next(this.books);
+  }
+
+  saveBooks() {
+    firebase.database().ref('/books').set(this.books);
+  }
+
+  getBooks() {
+    firebase.database().ref('/books')
+      .on('value', (data: DataSnapshot) => {
+        this.books = data.val() ? data.val() : [];
+        this.emitBooks();
+      }
+      );
+  }
+
+  getSingleBook(id: number) {
+    return new Promise(
+      (resolve, reject) => {
+        firebase.database().ref('/books/' + id).once('value').then(
+          (data: DataSnapshot) => {
+            resolve(data.val());
+          }, (error) => {
+            reject(error);
+          }
+        );
+      }
+    );
+  }
 }
